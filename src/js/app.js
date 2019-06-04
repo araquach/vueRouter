d2ecf10b6baba84f284747883cf51c161e0d8a51
Adam Carter <adamcarter@jakatasalon.co.uk>
2019-06-04 17:34:44 +0100

    getting ready to use router

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,8 @@
 import Vue from "vue";
+import VueRouter from 'vue-router';
 import TestComponent from './components/TestComponent.vue';
+
+Vue.use(VueRouter)
 
 Vue.component('test-component', TestComponent);
 
--- src/js/components/TestComponent.vue
+++ src/js/components/TestComponent.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h1 class="title is-3">Hello!</h1>
+        <h1 class="title is-3 has-text-primary">Hello!</h1>
         <p>Whats up!</p>
         <p>This is a test</p>
     </div>
