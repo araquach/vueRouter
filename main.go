f45236a4c3a4344d73dce81453725355ae272543
Adam Carter <adamcarter@jakatasalon.co.uk>
2019-06-04 16:00:07 +0100

    Go the layout set

--- main.go
+++ main.go
@@ -2,31 +2,27 @@ package main
 
 import (
 	"github.com/gorilla/mux"
+	"html/template"
 	"net/http"
 )
 
+var homeTemplate *template.Template
+
 func main() {
+	var err error
+	homeTemplate, err = template.ParseFiles("views/home.gohtml")
+	if err != nil {
+		panic(err)
+	}
 	r := mux.NewRouter()
-	r.HandleFunc("/", home).Methods("GET")
-
-	// Styles
-	assetHandler := http.FileServer(http.Dir("./dist/"))
-	assetHandler = http.StripPrefix("/dist/", assetHandler)
-	r.PathPrefix("/dist/").Handler(assetHandler)
-
-	// JS
-	jsHandler := http.FileServer(http.Dir("./dist/"))
-	jsHandler = http.StripPrefix("/dist/", jsHandler)
-	r.PathPrefix("/dist/").Handler(jsHandler)
-
-	//Images
-	imageHandler := http.FileServer(http.Dir("./dist/images/"))
-	r.PathPrefix("/dist/images/").Handler(http.StripPrefix("/dist/images/", imageHandler))
-
-	http.ListenAndServe(":5050", r)
+	r.HandleFunc("/", home)
+	http.ListenAndServe(":3050", r)
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	
+	if err := homeTemplate.Execute(w, nil); err != nil {
+		panic(err)
+	}
 }
+
